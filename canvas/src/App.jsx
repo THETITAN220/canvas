36a1450f4828aa39c85e657944ec8ca0b18eb123
nandan-19 <nandanks058@gmail.com>
2024-11-14 13:54:30 +0530

    .

--- canvas/src/App.jsx
+++ canvas/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter , Routes, Route } from 'react-router-dom'
 import './App.css'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
+import appointment from './pages/appointment'
 
 function App() {
 
