b20e372ddfa90bc289f742ef10bf6690da23c4de
Nikhil002531 <nikhil9532007@gmail.com>
2024-11-14 16:59:39 +0530

    landing page completed

--- canvas/src/pages/Landing.jsx
+++ canvas/src/pages/Landing.jsx
@@ -10,6 +10,14 @@ export default function Landing(){
         <Nav />
         <main className="main-content">
             <Carousel/>
+            <div className="about-section">
+    <h2>About HealthHub</h2>
+    <p>
+        Welcome to <strong>HealthHub</strong>, your streamlined solution for managing healthcare appointments and finding specialized doctors. HealthHub simplifies the process of connecting with medical professionals by offering a platform where you can browse doctor profiles, view their specialties, and book appointments seamlessly.
+        <br/><br/>
+        Our focus is to make healthcare accessible and convenient, ensuring you have all the information needed to choose the right healthcare provider. With HealthHub, finding expert care and booking appointments has never been easier. Take control of your health journey with HealthHub—<span>your partner in well-being</span>.
+    </p>
+</div>
         </main>
         <Footer />
         </div>
