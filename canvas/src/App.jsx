057a0f81e6deb657f055604af744a8236d507add
Raunak Rose <raunakrode220cool@gmail.com>
2024-11-14 13:19:01 +0530

    Update App.jsx

--- canvas/src/App.jsx
+++ canvas/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter , Routes, Route, useNavigate } from 'react-router-dom'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
 import './App.css'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
