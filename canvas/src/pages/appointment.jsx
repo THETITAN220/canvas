b5eb1f7dfe17abaa7863ddb7b9725760372b53fb
nandan-19 <nandanks058@gmail.com>
2024-11-14 15:30:11 +0530

    .

--- canvas/src/pages/appointment.jsx
+++ canvas/src/pages/appointment.jsx
@@ -1,17 +1,27 @@
- 
-
+  import react from 'react'
 export default function appointment() {
   return (
      <>
      <main>
         <div className="appointment">
         <input type="text" class="search-box" placeholder="Search"/>
-        <button className="img1"></button>
+        <div className="specialist">
+
+        <img src="canvas\src\assets\cardio.jpeg" alt="Cardio" className="cardio" />
+        <img src="canvas\src\assets\neuro.jpeg" alt="neuro" className="neuro" />
+        <img src="canvas\src\assets\ortho.png" alt="ortho" className="ortho" />
+        <img src="canvas\src\assets\phsyciatry.png" alt="phy" className="phy" />
+
+        </div>
         
         </div>
         <div className="gallery">
             <div className="specialist">
-            
+            <div className="intro"
+            ></div>
+            <div className="indetail">
+
+            </div>
             </div>
         </div>
      </main>
